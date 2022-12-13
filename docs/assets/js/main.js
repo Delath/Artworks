a5d374b253b7bb3e41141f5455f65f8331a30860
Dario <simoni.dario1@gmail.com>
2022-12-13 21:25:19 +0100

    loading transition time changed

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(function(){
       pageObscurer.style.width = "0px"
       pageObscurer.style.height = "0px"  
-    }, 1100)
+    }, 1200)
   }, 100)
 
 });
