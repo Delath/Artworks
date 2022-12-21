f903603653f36777661966db86be7777ed065cb5
Dario <simoni.dario1@gmail.com>
2022-12-21 23:07:42 +0100

    pwa debugging

--- docs/assets/sw.js
+++ docs/assets/sw.js
@@ -8,7 +8,7 @@ var APP_PREFIX = 'Artworks';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_04';
+var VERSION = 'version_05';
 
 var CACHE_NAME = APP_PREFIX + VERSION
 
